2b9fef4f813b2e19221bdb521b1eeae6386a6180
Ernando Rezende <ernando.03@gmail.com>
2025-03-22 01:15:13 +0000

    Creted a simple audio player

---
+++ src/main/java/br/com/devcompleto/SimpleAudioPlayer.java
@@ -0,0 +1,132 @@
+package br.com.devcompleto;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SimpleAudioPlayer {
+
+    Long currentFrame;
+    String status;
+    Clip clip;
+    AudioInputStream audioInputStream;
+    static String filePath;
+
+    public SimpleAudioPlayer() throws LineUnavailableException, UnsupportedAudioFileException, IOException {
+        audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+
+        clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        System.out.println("My Music");
+        filePath = "O Livro de Enoque_001.wav";
+        SimpleAudioPlayer audioPlayer = new SimpleAudioPlayer();
+
+        audioPlayer.play();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. pause");
+            System.out.println("2. resume");
+            System.out.println("3. restart");
+            System.out.println("4. stop");
+            System.out.println("5. Jump to specific time");
+            int c = scanner.nextInt();
+            audioPlayer.gotoChoice(c);
+            if (c == 4)
+                break;
+        }
+
+    }
+
+    private void gotoChoice(int c) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        switch (c)
+        {
+            case 1:
+                pause();
+                break;
+            case 2:
+                resumeAudio();
+                break;
+            case 3:
+                restart();
+                break;
+            case 4:
+                stop();
+                break;
+            case 5:
+                System.out.println("Enter time (" + 0 +
+                        ", " + clip.getMicrosecondLength() + ")");
+                Scanner sc = new Scanner(System.in);
+                long c1 = sc.nextLong();
+                jump(c1);
+                break;
+
+        }
+    }
+
+    private void jump(long c) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        if (c > 0 && c < clip.getMicrosecondLength())
+        {
+            clip.stop();
+            clip.close();
+            resetAudioStream();
+            currentFrame = c;
+            clip.setMicrosecondPosition(c);
+            this.play();
+        }
+    }
+
+    private void restart() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        clip.stop();
+        clip.close();
+        resetAudioStream();
+        currentFrame = 0L;
+        clip.setMicrosecondPosition(0);
+        this.play();
+    }
+
+    private void resumeAudio() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        if(status.equals("play")) {
+            System.out.println("Audio is already being played");
+            return;
+        }
+        clip.close();
+        resetAudioStream();
+        clip.setMicrosecondPosition(currentFrame);
+        this.play();
+    }
+
+    private void resetAudioStream() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+        clip.open(audioInputStream);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    private void pause() {
+        if (status.equals("paused")) {
+            System.out.println("audio is already paused");
+            return;
+        }
+        this.currentFrame = this.clip.getMicrosecondPosition();
+        this.stop();
+        status = "paused";
+    }
+
+    private void stop() {
+        currentFrame = 0L;
+        clip.stop();
+        clip.close();
+    }
+
+    private void play() {
+        clip.start();
+
+        status = "paly";
+    }
+}
