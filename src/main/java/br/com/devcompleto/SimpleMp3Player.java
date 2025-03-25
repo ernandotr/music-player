38c8074abf74c3ea0cc98756a72591b21ed06687
Ernando Rezende <ernando.03@gmail.com>
2025-03-25 22:17:05 +0000

    Improvents on SimpleMp3Player class

--- src/main/java/br/com/devcompleto/SimpleMp3Player.java
+++ src/main/java/br/com/devcompleto/SimpleMp3Player.java
@@ -8,11 +8,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.logging.Logger;
 
 public class SimpleMp3Player {
+    private static final Logger logger = Logger.getLogger("SimpleMp3Player");
 
     public static void main(String[] args) {
-        FileInputStream fis = null;
+        FileInputStream fis;
         try {
             String pathname = "Enoque_001.mp3";
             long audioSize = Files.size(Path.of(pathname));
@@ -20,13 +22,14 @@ public class SimpleMp3Player {
             System.out.printf("Size: %.2f Kb. %n",  audioSize / 1024.0 );
             fis = new FileInputStream(pathname);
 
-            System.out.println("Playing: " + pathname);
+            System.out.println();
+            logger.info("Playing: " + pathname);
             BufferedInputStream bis = new BufferedInputStream(fis);
             Player player = new Player(bis);
             player.play();
 
         } catch (IOException | JavaLayerException e) {
-            throw new RuntimeException(e);
+            logger.severe(e.getMessage());
         }
 
     }
